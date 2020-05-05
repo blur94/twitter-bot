d2f180ccc6fa639ee478d69799c663b9064dddc2
Ebube <dbupanyiam@gmail.com>
2020-05-05 14:27:18 -0700

    Delete config.js
    File contained private keys

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-
-module.exports = {
-  consumer_key: "S4ZL7UGQsxz6ymKLgMSIpzxnO",
-  consumer_secret: "UgXQg5AGGR1DdFrrDF72JR2JIKqHxaUdKhiAqB6jCfeR0OPhS3",
-  access_token_key: "1056622740785061889-WQmtXbnghs6o4DmUAKkZnYhhFUjwsB",
-  access_token_secret: "6w23ur5QHWCSvxwHyb3ZCgHIPXvvCIhXkb7OGPxHdLsQa",
-};
